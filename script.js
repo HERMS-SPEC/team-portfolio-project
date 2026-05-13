bd2c5f183f486a477477c86912b257e1c83197d1
Plasmatic241 <ll.leolion2@gmail.com>
2026-05-12 22:02:13 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("hermiloBtn").onclick = function () {
+    window.location.href = "HermiloProfile.html";
+};
+
+document.getElementById("leeBtn").onclick = function () {
+    window.location.href = "LeeProfile.html";
+};
+
+document.getElementById("hendryBtn").onclick = function () {
+    window.location.href = "hendry.html";
+};
+
+document.getElementById("fizaBtn").onclick = function () {
+    window.location.href = "Fizarashid.html";
+};
